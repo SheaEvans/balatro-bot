ee7fb6d9bdd3efc5e4e61ced88ee33e5283159d6
Shea Evans <ceva476@wgu.edu>
2025-01-23 14:54:17 -0800

    A basic initialation of a Rust Project that outputs hello world to the terminal

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+
+// vv Basic Comment design.
+/*
+ * Function: 
+ * Description: 
+ * Parameters:
+ */
+
+ /*
+ * Function: print welcome
+ * Description: outputs hello world in the command line
+ * Parameters: n/a
+ */
+
+ fn print_welcome() {
+    println!("Hello, world!");
+ }
+
+ /*
+ * Function: main
+ * Description: this is the heart of the operation
+ * Parameters: n/a
+ */
+fn main() {
+
+    // outputs hello world in the command line
+    print_welcome();
+}
