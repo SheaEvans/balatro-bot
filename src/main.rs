8065ecc9d3e3524e9e479b2740cb000cfd16e188
Shea Evans <ceva476@wgu.edu>
2025-01-23 15:13:05 -0800

    created a basic structure of development plan

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,63 @@
  */
 
  /*
- * Function: print welcome
- * Description: outputs hello world in the command line
- * Parameters: n/a
- */
-
- fn print_welcome() {
-    println!("Hello, world!");
- }
-
- /*
  * Function: main
  * Description: this is the heart of the operation
  * Parameters: n/a
  */
 fn main() {
 
-    // outputs hello world in the command line
-    print_welcome();
+    // basic structure of how I am going to design the data base using comments
+
+
+    /*
+     * initialize_bot(){
+     * 
+     *      // starts the game for the user
+     *      boot_balatro();
+     * 
+     *      // generate the screens overlay to communicate two ways with the user
+     *      generate_overlay();
+     *       
+     *      // generate information to help make the bot able to act faster (e.g. joker information sheets)  
+     *      generate_library_of_information_to_make_gameplay_faster();
+     * 
+     * }
+     *          
+     * 
+     * run_gameplay_bot(){
+     * 
+     *      //run the bot till the game ends 
+     *      loop {
+     * 
+     *          //display possible chip count for the best possible hand that could be played
+     *          display_best_hand();
+     *          
+     *          // create a list of actions the user could do to set the course of the run
+     *          // e.g. build towards straights, flushes, look for just the best course of action, etc.
+     *          get_user_input(){
+     *          
+     *              //processes the users requests and makes the best decision.
+     *              process_user_information();
+     *              
+     *          }
+     *       
+     *          // applys the users inputs and plays the best moves, accordingly until the next time that the users inoput is required.
+     *          apply_user_input(){
+     *              
+     *          }
+     * 
+     *      }
+     * 
+     * }
+     * 
+     * //apply the last game information to update strategies, and best calls.
+     * analyze_previous_gameplay(){
+     * 
+     * }
+     */
+
+
+
 }
+
